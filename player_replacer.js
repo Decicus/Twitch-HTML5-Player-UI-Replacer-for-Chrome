bd07b49b817fde02462c11a75ba1f637d5942897
Alex Thomassen <alex@thomassen.xyz>
2015-07-31 18:13:58 +0200

    Update to 1.0.1

--- player_replacer.js
+++ player_replacer.js
@@ -1,6 +1,18 @@
-$( window ).load( function() {
-    console.log( "** HTML5 Twitch Player Replacer has executed!" );
+function replacePlayer() {
+    console.log( "** HTML5 Twitch Player Replacer - Executed" );
+    var path = window.location.pathname.split( '/' );
+    var channel = path[ 1 ];
+    var optional = path[ 2 ]; // Secondary parameter, specifically to replace it in the dashboard.
+    var playerFrame = $( "<iframe>" ).attr( "src", "http://player.twitch.tv/?branding=false&showInfo=false&channel=" + channel ).attr( "width", "100%" ).attr( "height", "100%" ).css( "border", 0 );
     setTimeout( function() {
-        $(".js-player").html($("<iframe>").attr("src", "http://player.twitch.tv/?branding=false&showInfo=false&channel="+window.location.pathname.substr(1)).attr("width", "100%").attr("height", "100%").css("border", 0));
+        if( optional == "dashboard" ) {
+            $( '.live_site_player_container' ).html( playerFrame );
+        } else if( optional === undefined ) {
+            $( ".js-player" ).html( playerFrame );
+        }
     }, 500 );
+}
+
+$( window ).load( function() {
+    replacePlayer();
 } );
