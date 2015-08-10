fa875e1524e39d62fe21a8324723841a84e2f8d5
Alex Thomassen <alex@thomassen.xyz>
2015-08-10 23:35:08 +0200

    1.0.5
    Fixed some broken player replacing.

--- inject.js
+++ inject.js
@@ -1,6 +1,3 @@
 var s = document.createElement( 'script' );
 s.src = chrome.extension.getURL( 'player_replacer.js' );
 ( document.head || document.documentElement ).appendChild( s );
-s.onload = function() {
-    s.parentNode.removeChild( s );
-};
